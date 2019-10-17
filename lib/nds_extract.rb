f9b6166c239948529a3fa789e5c5d025aa613bdc
Wills Blake <richwblake@gmail.com>
2019-10-17 01:38:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,10 +43,9 @@ end
 def total_gross(source)
   total_gross_of_earnings = 0
   director_index = 0
-  director_list = list_of_directors( source )
   
   while director_index < source.size do
-    total_gross_of_earnings += gross_for_director[director_list[director_index]]
+    total_gross_of_earnings += gross_for_director[]
   end
 
 end
