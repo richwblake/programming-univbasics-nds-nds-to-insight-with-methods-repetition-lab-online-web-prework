21abeb9c29153672f3273369ec36fe3365d5e0e8
Wills Blake <richwblake@gmail.com>
2019-10-17 01:30:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 # Takes director NDS as argument
+# Returns a hash with director names (strings) as keys, and respective gross totals as values
 def directors_totals(source)
   result = {}
   director_index = 0
